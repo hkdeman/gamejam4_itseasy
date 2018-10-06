c0fbd0a81a5b162e39f06738adcc750bc252dd7f
modelorona <angel.hristozov@gmail.com>
2018-10-06 01:11:53 +0100

    added crude timer

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour {
+
+    public float time = 60.0f; // in seconds
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DownBy(1);
+        if (time <= 0)
+        {
+            // do whatever needs to happen when the game is over
+            Debug.Log("under 0: " + time);
+        }
+    }
+
+    void DownBy(float amount)
+    {
+        time -= Time.deltaTime * amount;
+    }
+
+}
