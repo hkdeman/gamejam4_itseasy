4fb5a88dbd9eff39fd94e1dbf18bb67fa237be07
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2018-10-06 19:34:02 +0100

    add right rotation

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -134,6 +134,7 @@ public class Map : MonoBehaviour
 		}
 
 		Debug.Log(("Pass"));
+		rotate("right");
 		myCharacter.GetComponent<Character>().MoveRight();
 		
 		return true;
