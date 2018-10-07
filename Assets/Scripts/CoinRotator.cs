c31768bd88ee303db46920c4c75c953e7bfcab14
Hemang Kandwal <hemangkandwal@gmail.com>
2018-10-07 13:07:34 +0100

    Added all the boss levels.

--- Assets/Scripts/CoinRotator.cs
+++ Assets/Scripts/CoinRotator.cs
@@ -7,12 +7,25 @@ public class CoinRotator : MonoBehaviour
 
     bool rotating;
     bool collected;
+
+    GameObject mapObject;
+    GameObject timerGameObject;
+    TimerManager timer;
+    Map map;
+
+    public GameObject particles;
+
     // Use this for initialization
     void Start()
     {
         rotating = false;
         collected = false;
         StartCoroutine(RotateMe(Vector3.up * 180, 2.0f));
+        mapObject = GameObject.FindWithTag("Map");
+        map = mapObject.GetComponent<Map>();
+        //particles = GameObject.FindWithTag("ParticleSystem");
+        timerGameObject = GameObject.FindWithTag("Timer");
+        timer = timerGameObject.GetComponent<TimerManager>();
     }
 
     IEnumerator RotateMe(Vector3 byAngles, float inTime)
@@ -28,4 +41,34 @@ public class CoinRotator : MonoBehaviour
             }
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(particles, gameObject.transform.position, gameObject.transform.rotation);
+        if (map.currentLevel == 4)
+        {
+            timer.time += 15.0f;
+            float x = GetAValueApartFrom(0, map.COLS - 2, Mathf.RoundToInt(other.gameObject.transform.position.x)) * map.SCALE;
+            float z = GetAValueApartFrom(0, map.COLS - 2, Mathf.RoundToInt(other.gameObject.transform.position.z)) * map.SCALE;
+            Instantiate(gameObject, new Vector3(x, 2f, z), gameObject.transform.rotation);
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private int GetAValueApartFrom(int min, int max, int avoid)
+    {
+        int value = 0;
+        while (true)
+        {
+            value = UnityEngine.Random.Range(min, max - 1);
+            if (value != avoid) break;
+        }
+        return value;
+    }
+
 }
