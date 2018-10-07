f02d56bcc415734af01590dd942072c2e67c0614
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2018-10-07 02:45:39 +0100

    Import TexMesh Pro !! And story text

--- Assets/Scripts/RandomWord.cs
+++ Assets/Scripts/RandomWord.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Random : MonoBehaviour
+public class RandomWord : MonoBehaviour
 {
 	public Text play;
 //	public Text options;
