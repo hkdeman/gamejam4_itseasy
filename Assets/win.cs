40d2d3f635dae332ae8a058719103fe4c9dc9bb3
Andrei Serguienko <andrei.serguienko@etu.univ-amu.fr>
2018-10-07 13:13:48 +0100

    win screen

---
+++ Assets/win.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class win : MonoBehaviour
+{
+	public GameObject pannel;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void won()
+	{
+		pannel.SetActive(true);
+	}
+}
