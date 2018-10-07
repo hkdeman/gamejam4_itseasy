8dde2084dce4942afa863c80d4631702b1fbe8e3
Hemang Kandwal <hemangkandwal@gmail.com>
2018-10-07 02:36:55 +0100

    Added orientation locks.

--- Assets/Scripts/MainCameraRotator.cs
+++ Assets/Scripts/MainCameraRotator.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class MainCameraRotator : MonoBehaviour {
 
     public GameObject mapGameObject;
+    public bool allowOrientationOne = false;
+    public bool allowOrientationTwo = false;
+    public bool allowOrientationThree = false;
+    public bool allowOrientationFour = false;
 
     Camera cam;
     Map map;
 
     private bool rotating;
+    private int currentRotation;
 
 	// Use this for initialization
 	void Start () {
         rotating = false;
+        currentRotation = 0;
         map = mapGameObject.GetComponent<Map>();
         transform.position = new Vector3((map.ROWS/2)*map.SCALE, 0, (map.COLS/2)*map.SCALE-2);
 
@@ -26,8 +32,8 @@ public class MainCameraRotator : MonoBehaviour {
     {
         rotating = true;
         var fromAngle = transform.rotation;
-        var toAngle = Quaternion.Euler(transform.eulerAngles + byAngles);
-        for (var t = 0f; t < 1; t += Time.deltaTime / inTime)
+        var toAngle = Quaternion.Euler(transform.eulerAngles) * Quaternion.Euler(byAngles);
+        for (var t = 0f; t <=1; t += Time.deltaTime / inTime)
         {
             transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
             yield return null;
@@ -38,9 +44,22 @@ public class MainCameraRotator : MonoBehaviour {
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && !rotating) {
-            StartCoroutine(RotateMe(Vector3.up * -90.62f, 0.3f));
+            if((currentRotation == 0 && allowOrientationFour ) || 
+               (currentRotation == 1 && allowOrientationOne) || 
+               (currentRotation == 2 && allowOrientationTwo) || 
+               (currentRotation == 3 && allowOrientationThree)) {
+                currentRotation = currentRotation>0 ? (currentRotation-1) : 3;
+                StartCoroutine(RotateMe(new Vector3(0, -90f, 0), 0.3f));
+            }
         } else if (Input.GetKeyDown(KeyCode.Q) && !rotating) {
-            StartCoroutine(RotateMe(Vector3.up * 90.62f, 0.3f));
+            if ((currentRotation == 0 && allowOrientationTwo) ||
+               (currentRotation == 1 && allowOrientationThree) ||
+               (currentRotation == 2 && allowOrientationFour) ||
+               (currentRotation == 3 && allowOrientationOne))
+            {
+                currentRotation = (currentRotation + 1) % 4;
+                StartCoroutine(RotateMe(new Vector3(0, 90f, 0), 0.3f));
+            }
         }
     }
 }
